849a234f4e77f0a59acd7feeb490cc13090ec954
upendraabaris <upendra.abaris@gmail.com>
2025-08-26 13:12:10 +0530

    config chnages

--- src/config/apiConfig.tsx
+++ src/config/apiConfig.tsx
@@ -1,6 +1,6 @@
 // const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
-const BASE_URL =  "http://127.0.0.1:8000/api";
+const BASE_URL =  "http://127.0.0.1:8005/api";
 
 // const BASE_URL = "http://shcanary.platinum-infotech.com:8000/api";
 
